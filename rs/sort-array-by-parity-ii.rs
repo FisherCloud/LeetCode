ac38cac1397d386737cf8c0ad0ac76323e1fa09c
FisherCloud <FisherCloud@qq.com>
2020-01-04 14:50:38 +0800

    optimization 922

--- rs/sort-array-by-parity-ii.rs
+++ rs/sort-array-by-parity-ii.rs
@@ -4,7 +4,7 @@
  * @Email: fishercloud@qq.com
  * @Github: https://github.com/FisherCloud
  * @Date: 2020-01-04 14:28:29
- * @LastEditTime: 2020-01-04 14:38:09
+ * @LastEditTime: 2020-01-04 14:47:45
  */
 
 #[derive(Debug)]
@@ -16,18 +16,36 @@ use std::collections::VecDeque;
 impl Solution {
     pub fn sort_array_by_parity_ii(a: Vec<i32>) -> Vec<i32> {
         let mut ans: Vec<i32> = vec![];
-        let mut queue: VecDeque<i32> = VecDeque::new();
+        // 方法一，队列，12ms
+        // let mut queue: VecDeque<i32> = VecDeque::new();
+        // for item in a {
+        //     if 0 == item & 0x01 {
+        //         queue.push_front(item); // 偶数放前面
+        //     } else {
+        //         queue.push_back(item); // 奇数放后面
+        //     }
+        // }
+
+        // while !queue.is_empty() {
+        //     ans.push(queue.pop_front().unwrap()); // 偶数位置放偶数
+        //     ans.push(queue.pop_back().unwrap()); // 奇数位置放奇数
+        // }
+        
+        // 方法二，容器，4ms
+        let mut odds: Vec<i32> = vec![];
+        let mut evens: Vec<i32> = vec![];
+
         for item in a {
             if 0 == item & 0x01 {
-                queue.push_front(item); // 偶数放前面
+                evens.push(item);
             } else {
-                queue.push_back(item); // 奇数放后面
+                odds.push(item);
             }
         }
 
-        while !queue.is_empty() {
-            ans.push(queue.pop_front().unwrap()); // 偶数位置放偶数
-            ans.push(queue.pop_back().unwrap()); // 奇数位置放奇数
+        while !odds.is_empty() {
+            ans.push(evens.pop().unwrap());
+            ans.push(odds.pop().unwrap());
         }
 
         (ans)
