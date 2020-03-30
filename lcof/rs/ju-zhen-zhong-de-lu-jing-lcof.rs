2defcf4f80049ec9adcbdbd22ff9cb2fdd99ea46
FisherCloud <fishercloud@qq.com>
2020-03-30 20:55:56 +0800

    alter lcof-12 with rust

--- lcof/rs/ju-zhen-zhong-de-lu-jing-lcof.rs
+++ lcof/rs/ju-zhen-zhong-de-lu-jing-lcof.rs
@@ -7,35 +7,38 @@ struct Solution {
 // DFS(Depth First Search)
 impl Solution {
     pub fn exist(board: Vec<Vec<char>>, word: String) -> bool {
-        if board.is_empty() || word.is_empty() {
+        if board.is_empty() || word.is_empty(){
             return false;
         }
-        let mut mutBoard = board;
-        let height = mutBoard.len();
-        let width = mutBoard[0].len();
-        for i in 0..height {
-            for j in 0..width {
-                return Self::dfs(&mut mutBoard, word, i, j);
+        let mut mut_board = board;
+        let height = mut_board.len() as i32;
+        let width = mut_board[0].len() as i32;
+        for x in 0..height {
+            for y in 0..width {
+                if Self::dfs(&mut mut_board, word.to_string(), x, y) {
+                    return true;
+                }
             }
         }
         return false;
     }
 
-    fn dfs(board: &mut Vec<Vec<char>>, word: String, x: usize, y: usize) -> bool {
+    fn dfs(board: &mut Vec<Vec<char>>, word: String, x: i32, y: i32) -> bool {
+        // println!("{:?}", (x, y));
         if word.is_empty() {
             return true;
         }
-        if x < 0 || x >= board.len() || y < 0 || y >= board[0].len() || word.chars().nth(0) != Some(board[x][y]) {
+        if x < 0 || y < 0 || x as usize >= board.len() || y as usize >= board[0].len() || word.chars().nth(0) != Some(board[x as usize][y as usize]) {
             return false;
         }
-        let tmp = board[x][y];
-        board[x][y] = '0';
+        let tmp = board[x as usize][y as usize];
+        board[x as usize][y as usize] = '0';
         let res = Self::dfs(board, word[1..].to_string(), x + 1, y) || Self::dfs(board, word[1..].to_string(), x, y + 1) || Self::dfs(board, word[1..].to_string(), x - 1, y) || Self::dfs(board, word[1..].to_string(), x, y - 1);
-        board[x][y] = tmp;
+        board[x as usize][y as usize] = tmp;
         return res;
     }
 }
 
 fn main() {
-    unimplemented!();
+    println!("{:?}", Solution::exist(vec![vec!['a','b']], "ba".to_string()));
 }
