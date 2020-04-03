b302a3b6f4c8d02bbb71b0cc9647a3595540317d
FisherCloud <fishercloud@qq.com>
2020-04-03 21:17:51 +0800

    add 1195  with cpp

---
+++ problems/cc/fizz-buzz-multithreaded.cc
@@ -0,0 +1,106 @@
+// https://leetcode-cn.com/problems/fizz-buzz-multithreaded/
+#include <condition_variable>
+#include <functional>
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+using namespace std;
+
+class FizzBuzz {
+   private:
+    int n;
+    int count = 1;
+    mutex mtx;
+    condition_variable cv;
+
+   public:
+    FizzBuzz(int n) { this->n = n; }
+
+    // printFizz() outputs "fizz".
+    void fizz(function<void()> printFizz) {
+        unique_lock<mutex> lock(mtx);
+        while (count <= n) {
+            cv.wait(lock, [this]() {
+                return count > n || count % 3 == 0 && count % 5 != 0;
+            });
+            if (count > n) break;
+            printFizz();
+            ++count;
+            cv.notify_all();
+        }
+        cv.notify_all();
+    }
+
+    // printBuzz() outputs "buzz".
+    void buzz(function<void()> printBuzz) {
+        unique_lock<mutex> lock(mtx);
+        while (count <= n) {
+            cv.wait(lock, [this]() {
+                return count > n || count % 3 != 0 && count % 5 == 0;
+            });
+            if (count > n) break;
+            printBuzz();
+            ++count;
+            cv.notify_all();
+        }
+        cv.notify_all();
+    }
+
+    // printFizzBuzz() outputs "fizzbuzz".
+    void fizzbuzz(function<void()> printFizzBuzz) {
+        unique_lock<mutex> lock(mtx);
+        while (count <= n) {
+            cv.wait(lock, [this]() {
+                return count > n || count % 3 == 0 && count % 5 == 0;
+            });
+            if (count > n) break;
+            printFizzBuzz();
+            ++count;
+            cv.notify_all();
+        }
+        cv.notify_all();
+    }
+
+    // printNumber(x) outputs "x", where x is an integer.
+    void number(function<void(int)> printNumber) {
+        unique_lock<mutex> lock(mtx);
+        while (count <= n) {
+            cv.wait(lock, [this]() {
+                return count > n || count % 3 != 0 && count % 5 != 0;
+            });
+            if (count > n) break;
+            printNumber(count);
+            ++count;
+            cv.notify_all();
+        }
+        cv.notify_all();
+    }
+};
+
+int main(int argc, char const *argv[]) {
+    auto fizzbuzz = new FizzBuzz(15);
+
+    function<void()> printFizz = [] { cout << "fizz" << endl; };
+    function<void()> printBuzz = [] { cout << "buzz" << endl; };
+    function<void()> printFizzBuzz = [] { cout << "fizzbuzz" << endl; };
+    function<void(int)> printNumber = [](int count) { cout << count << endl; };
+
+    // printFizz();
+    // printBuzz();
+    // printFizzBuzz();
+    // printNumber(10);
+
+    // error
+    // thread tfizz(fizzbuzz->fizz, printFizz);
+    // thread tbuzz(fizzbuzz->buzz, printBuzz);
+    // thread tfizzbuzz(fizzbuzz->fizzbuzz, printFizzBuzz);
+    // thread tnumber(fizzbuzz->number, printNumber);
+
+    // tfizz.join();
+    // tbuzz.join();
+    // tfizzbuzz.join();
+    // tnumber.join();
+    delete fizzbuzz;
+    return 0;
+}
